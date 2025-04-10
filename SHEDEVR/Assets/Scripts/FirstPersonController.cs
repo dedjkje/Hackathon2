c30de9276b8239547faa521c91d26217f1a4d7c4
AlexeiTaraska <ifyoualef@gmail.com>
2025-04-10 18:21:16 +0300

    ready

---
+++ SHEDEVR/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,125 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [Header("Параметры персонажа")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpPower;
+    [SerializeField] float gravityPower;
+
+    [Header("Джойстик")]
+    [SerializeField] Joystick joystick;
+
+    [Header("Камера")]
+    [SerializeField] float cameraSensivity;
+    [SerializeField] Transform cameraTransform;
+
+    private float gravityForce;
+    private Vector3 moveVector;
+    private CharacterController characterController;
+    private int rightFingerId;
+    private float halfScreenWidth;
+    private Vector2 lookInput;
+    private float cameraPitch;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        rightFingerId = -1;
+        halfScreenWidth = Screen.width / 2;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        GamingGravity();
+
+        if (rightFingerId != -1)
+        {
+            LookAround();
+        }
+    }
+
+    void Update()
+    {
+        GetTouchInput();
+    }
+
+    private void GetTouchInput()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch t = Input.GetTouch(i);
+
+            switch (t.phase)
+            {
+                case TouchPhase.Began:
+                    if (t.position.x > halfScreenWidth && rightFingerId == -1)
+                    {
+                        rightFingerId = t.fingerId;
+                    }
+                    break;  
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    if (t.fingerId == rightFingerId)
+                    {
+                        rightFingerId = -1;
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (t.fingerId == rightFingerId)
+                    {
+                        lookInput = t.deltaPosition * cameraSensivity * Time.deltaTime;
+                    }
+                    break;
+                case TouchPhase.Stationary:
+                    if (t.fingerId == rightFingerId)
+                    {
+                        lookInput = Vector2.zero;
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void LookAround()
+    {
+        cameraPitch = Mathf.Clamp(cameraPitch - lookInput.y, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+
+        transform.Rotate(transform.up, lookInput.x);
+    }
+
+    private void MovePlayer()
+    {
+        moveVector = Vector2.zero;
+        moveVector.x = joystick.Horizontal;
+        moveVector.z = joystick.Vertical;
+        moveVector.y = gravityForce;
+
+        moveVector = transform.right * moveVector.x + transform.forward * moveVector.z + transform.up * moveVector.y;
+
+        characterController.Move(moveVector * moveSpeed * Time.deltaTime);
+    }
+
+    private void GamingGravity()
+    {
+        if(!characterController.isGrounded)
+        {
+            gravityForce -= gravityPower * Time.deltaTime;
+        }
+        else
+        {
+            gravityForce = -1f;
+        }
+    }
+
+    public void onClickJump()
+    {
+        if (characterController.isGrounded)
+        {
+            gravityForce = jumpPower;
+        }
+    }
+}
