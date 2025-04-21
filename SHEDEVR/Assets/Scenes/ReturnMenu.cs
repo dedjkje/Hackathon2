be06d9402dbc1cb8b61cc120bf0ab9e9ef26159f
AlexeiYM <alyakushevich.2006@gmail.com>
2025-04-21 09:16:51 +0300

    release

--- SHEDEVR/Assets/Scenes/ReturnMenu.cs
+++ SHEDEVR/Assets/Scenes/ReturnMenu.cs
@@ -7,7 +7,7 @@ public class ReturnMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        back();
+        StartCoroutine(back());
     }
 
     // Update is called once per frame
